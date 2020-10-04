658038518cff03aaf2989cc43e038323e85649bd
“rungsiman1” <rungsiman.j@psru.ac.th>
2020-10-04 19:12:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pong1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.pong1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Point;
+import android.view.Display;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private PongGame mPongGame;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        mPongGame = new PongGame(this, size.x, size.y);
+        setContentView(mPongGame);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPongGame.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPongGame.pause();
+    }
+}
